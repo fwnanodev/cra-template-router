5eb7406d1c1dd3179ad716d231ef2f161eaac434
jrthartwig <jocelynnhartwig@gmail.com>
2022-07-04 12:33:49 -0400

    fix: spelling of RootComponent

--- template/src/App.js
+++ template/src/App.js
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Layout from './components/Layout';
-import Home from './pages/Home/Home';
-import Contact from './pages/Contact/Contact';
+import RootComponent from './pages/RootComponent/RootComponent';
+import SecondComponent from './pages/SecondComponent/SecondComponent';
 import {ROOT_COMPONENT_ROUTE, SECOND_COMPONENT_ROUTE} from './route-constants';
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route index path={`${ROOT_COMPONENT_ROUTE}`} element={<Home />} />
-        <Route path={`${SECOND_COMPONENT_ROUTE}`} element={<Contact />} />
+        <Route index path={`${ROOT_COMPONENT_ROUTE}`} element={<RootComponent />} />
+        <Route path={`${SECOND_COMPONENT_ROUTE}`} element={<SecondComponent />} />
       </Routes>
     </Layout>
   );
--- template/src/pages/RootComponent/RootComponent.jsx
+++ template/src/pages/RootComponent/RootComponent.jsx
@@ -1,7 +1,7 @@
 import { ROOT_COMPONENT_NAME } from "../../route-constants";
 
-const RootCompoent = () => {
+const RootComponent = () => {
   return <div> {`${ROOT_COMPONENT_NAME} Page`}</div>;
 };
 
-export default RootCompoent;
+export default RootComponent;
