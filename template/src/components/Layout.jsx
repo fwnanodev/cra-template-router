b525f67018d9ad9db34f16086e08003acede560d
emilys314 <emilys314@protonmail.com>
2022-07-04 11:32:46 -0400

    formatting changes

--- template/src/components/Layout.jsx
+++ template/src/components/Layout.jsx
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <h1>
         Layout
       </h1>
 
       <nav>
-        <Link to="/">go to PageA (/)</Link>  
+        <Link to="/">go to PageA (/)</Link>
         <br />
         <Link to="/page-b">go to PageB (/page-b)</Link>
       </nav>
+
       {children}
-    </>
+    </div>
   );
 }
 
